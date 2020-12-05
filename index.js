76f10670565fe33f6d8de6a0e48816feb28a5ade
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2020-12-05 22:46:29 +0700

    init project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express')
+require('dotenv').config();
+const PORT=process.env.PORT || 8000
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
