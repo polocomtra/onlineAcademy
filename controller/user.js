c6f6092fea3989cc6cff4d2cad976e82fda4cc09
DANGKHOA <18120186@student.hcmus.edu.vn>
2021-01-06 02:57:55 +0700

    Show WistList for Student

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,7 @@
+const User = require('../model/User')
+const Course = require('../model/Course')
+const _ = require('lodash')
+
 exports.renderProfile = (req, res) => {
     res.render('user/profile', {
         user: res.locals.user,
@@ -13,4 +17,40 @@ exports.isPro = (req, res, next) => {
             error: "You are not authorized"
         })
     }
+}
+
+exports.renderAllCourses = (req, res) => {
+    res.render('user/learning/all-courses', {
+        user: res.locals.user
+    });
+}
+
+exports.renderCollection = (req, res) => {
+    res.render('user/learning/collection', {
+        user: res.locals.user
+    });
+}
+
+exports.renderWistlist = (req, res) => {
+    var length = res.locals.user.wistlist.length;
+    var wistlist =[];
+    for(var i = 0 ;i < length; i++)
+    {
+        wistlist[i] = res.locals.user.wistlist[i].course;
+    }
+    Course.find({_id : { "$in": wistlist }}).exec((err, course) => {
+        if (err) {
+            console.log(err)
+        }            
+        res.render('user/learning/wistlist', {
+            courses: course 
+        })
+    })
+}
+
+
+exports.renderArchived = (req, res) => {
+    res.render('user/learning/archived', {
+        user: res.locals.user
+    });
 }
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 const { renderProfile } = require('../controller/user');
 const { layoutMiddleWare } = require('../middlewares/layout');
+const { renderAllCourses } = require('../controller/user');
+const { renderCollection } = require('../controller/user');
+const { renderWistlist } = require('../controller/user');
+const { renderArchived } = require('../controller/user');
 
 router.get('/profile/:userId', layoutMiddleWare, renderProfile)
-
+router.get('/learning/all-courses', layoutMiddleWare, renderAllCourses)
+router.get('/learning/collection', layoutMiddleWare, renderCollection)
+router.get('/learning/wistlist', layoutMiddleWare, renderWistlist)
+router.get('/learning/archived', layoutMiddleWare, renderArchived)
 module.exports = router;
